b252452df7152dde88a0a6bd820a61ea53ac926b
nashmj <34590724+nashmj@users.noreply.github.com>
2017-12-16 11:09:14 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,23 +1,7 @@
-list=[]
-nterms=100
-nterms=int(input('enter the no of terms'))
-n1=0
-n2=1
-list.append(n1)
-list.append(n2)
-count=0
-print(nterms)
-if nterms==1:
-   print('the fibonacci series upto',nterms,":")
-   print(n1)
-   list.append(n1)
+n=int(input("enter the number"))
+if n<0:
+	print 'the number is negative'
+elif n==0:
+	print 'the number is 0'
 else:
-    print('the fibonacci series upto',nterms,":")
-    while count<nterms:
-    	print(n1)
-    	nxt=n1+n2
-    	n1=n2
-    	n2=nxt
-    	count+=1
-    	list.append(nxt)
-print list
+	print 'the number is possitive'
