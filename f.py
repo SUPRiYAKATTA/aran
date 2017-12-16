4e207a03b8cea297a7f440570bb47150fa0cdb01
nashmj <34590724+nashmj@users.noreply.github.com>
2017-12-16 15:22:43 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,7 +1,11 @@
-n=int(input("enter the number"))
-if n<0:
-	print 'the number is negative'
-elif n==0:
-	print 'the number is 0'
+n=input("enter the number")
+if (n.isnumeric()):
+	n=int(n)
+	if (n<0):
+		print("the number is negative")
+	elif (n==0):
+		print("the number is 0")
+	else:
+		print("the number is positive")
 else:
-	print 'the number is possitive'
+	print("invalid input")
