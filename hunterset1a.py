d2cd45be380706c3d8f526fd53dd0850235c600a
nashmj <34590724+nashmj@users.noreply.github.com>
2018-03-07 15:21:30 +0530

    Create hunterset1a.py

---
+++ hunterset1a.py
@@ -0,0 +1,17 @@
+x=int(input())
+    list=[]
+    list2=[]
+    s=[]
+    for i in range(x):
+        a=input()
+        list.append(a)
+    print(list)
+    for i in list:
+        if list.count(i)>1:
+            print(i)
+            list2.append(i)
+    list2.sort()
+    for i in list2:
+        if i not in s:
+            s.append(i)
+    print(s)
