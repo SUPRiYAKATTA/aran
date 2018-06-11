49afa21dcfd551f91c760a82240fa523f84da3df
nashmj <34590724+nashmj@users.noreply.github.com>
2018-06-11 12:13:24 +0530

    Create uniquenumber.py

---
+++ uniquenumber.py
@@ -0,0 +1,13 @@
+def main():
+    x=int(input())
+    list=[]
+    for i in range(x):
+        a=input()
+        list.append(a)
+    print(list)
+    for i in list:
+        if list.count(i)==1:
+            print(i)
+
+if __name__ == '__main__':
+    main()
