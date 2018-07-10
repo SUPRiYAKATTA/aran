0f550c03926e927c05b0dd881684d68599aa8af8
nashmj <34590724+nashmj@users.noreply.github.com>
2018-07-10 22:47:30 +0530

    Create leastdiv.py

---
+++ leastdiv.py
@@ -0,0 +1,25 @@
+def main():
+    a_arr=[]
+    b_arr=[]
+    c_arr=[]
+    a=int(input())
+    b=int(input())
+    if a==b:
+        print(a)
+    if (a or b)==0:
+        print("otha")
+    else:
+        for i in range(1,10000):
+            value=a*i
+            a_arr.append(value)
+        for i in range(1,10000):
+            value_1=b*i
+            b_arr.append(value_1)
+        for i in range(len(a_arr)):
+            if a_arr[i] in b_arr:
+                c_arr.append(a_arr[i])
+        print(c_arr[0])
+
+
+if __name__ == '__main__':
+    main()
